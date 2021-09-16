a4887a09bd6cf784d1bf27a30b88961d9c0690fc
Massimo Artizzu <maxart.x@gmail.com>
2021-09-16 10:53:02 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+const { readFileSync } = require('fs');
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const tap = require('gulp-tap');
+const cleanCSS = require('gulp-clean-css');
+const rimraf = require('rimraf');
+const browserSync = require('browser-sync');
+const ghPages = require('gh-pages');
+
+async function reload() {
+  return browserSync.reload();
+}
+
+function slides() {
+  return src('src/presentations/**/*.html')
+    .pipe(tap(file => {
+      const html = file.contents.toString();
+      const parsed = html
+        .replace(
+          /\bslide:(.+?)\s/g,
+          (m, source) => {
+            try {
+              const slide = readFileSync(`src/slides/${source}.html`, 'utf-8');
+              return slide;
+            } catch (e) {
+              console.error(`Slide not found: ${source}`);
+              return m;
+            }
+          }
+        );
+      file.contents = Buffer.from(parsed);
+    }))
+    .pipe(dest('public'));
+}
+exports.slides = slides;
+exports['slides+reload'] = series(slides, reload);
+
+function css() {
+  return src('src/styles/*.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(dest('public/css'), { sourcemaps: true })
+    .pipe(browserSync.stream());
+}
+exports.css = css;
+
+function js() {
+  // No compilation provided, just use ES modules.
+  // Provide your own compilation if you want, though.
+  return src('src/js/**/*.js')
+    .pipe(dest('public/js'));
+}
+exports.js = js;
+exports['js+reload'] = series(js, reload);
+
+const assets = () => src('static/**/*').pipe(dest('public'));
+const vendors = () => src([
+    'node_modules/p-slides/components/**/*.js',
+    'node_modules/p-slides/*.js',
+    'node_modules/p-slides/css/**/*.css',
+    'node_modules/prismjs/prism.js',
+    'node_modules/prismjs/components/prism-typescript.js',
+    'node_modules/prismjs/themes/prism-okaidia.css'
+  ], { base: './node_modules' })
+  .pipe(dest('public/vendor'));
+exports.static = parallel(assets, vendors);
+exports['static+reload'] = series(exports.static, reload);
+
+const clean = async () => rimraf.sync('public');
+exports.clean = clean;
+
+exports.default = parallel(
+  exports.static,
+  exports.css,
+  exports.slides,
+  exports.js
+);
+
+exports.serve = series(exports.default, () => {
+  browserSync.init({
+    ghostMode: false,
+    server: {
+      baseDir: './public'
+    }
+  });
+
+  watch('src/styles/**/*.scss', exports.css);
+  watch('src/{presentations,slides}/**/*.html', exports['slides+reload']);
+  watch('src/js/**/*.js', exports['js+reload']);
+  watch('static/**/*', exports['static+reload']);
+});
+
+exports.publish = series(exports.default, done => {
+  ghPages.publish('public', error => {
+    if (error) {
+      console.error('Error while publishing on GitHub pages', error);
+      done(Error(error));
+    } else {
+      console.log('Published on branch gh-pages');
+      done();
+    }
+  });
+});
+
+exports['watch:css'] = function watchCss() {
+  return watch('src/styles/**/*.scss', exports.css);
+};
+exports['watch:slides'] = function watchSlides() {
+  return watch('src/{presentation,slides}/*.html', exports.slides);
+};
+exports['watch:static'] = function watchStatic() {
+  return watch([ 'static/**/*' ], exports.static);
+};
+
+exports.watch = parallel(
+  exports['watch:static'],
+  exports['watch:css'],
+  exports['watch:slides']
+);
---
+++ readme.md
@@ -0,0 +1,24 @@
+Not just 'sites' anymore: the power of the web today
+====================================================
+
+Slides for a talk about the great beauty of frontend. # slides, about 30 minutes.
+
+Originally conceived for the [Codemotion DevCast](https://live.codemotion.com/devcast/la-grande-bellezza-del-frontend) (in Italian) online, 2021-10-12.
+
+Built on top of [P-Slides](https://github.com/MaxArt2501/p-slides)!
+
+## Building
+
+Node.js 10.x is required (actually it could work in Node 8 - haven't checked).
+
+To start the presentation in development mode:
+
+1. Clone the repository;
+2. `npm i`
+3. `npm run serve`
+
+Or `npm run build` to just build it, then serve `/public` with your HTTP server of choice.
+
+## Tests
+
+... what?
